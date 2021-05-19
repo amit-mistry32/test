0ff22139feaffc318afe259bb868b605f11bddbd
amit <amitmistry950@gmail.com>
2021-05-19 16:01:03 +0530

    Rename Learn React Again to Learn React Agains

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Again
+          Learn React Agains
         </a>
       </header>
     </div>
